e02258ab397996f90e0d40caa5f95d08949bacda
Tony Petty <tpetty@appmobi.com>
2013-05-24 09:34:06 -0400

    margin change

--- wp-content/themes/appmobi_mainsite_v1/footer.php
+++ wp-content/themes/appmobi_mainsite_v1/footer.php
@@ -25,7 +25,7 @@
 	</footer><!-- #colophon -->
 	<div id="appMobi_lightbox" style="display: none; overflow: hidden; position: absolute; top: 0; left: 0; right: 0; bottom: 0; z-index: 1000; background-color: rgba(0,0,0, 0.5);">
 		<div style="color: #fff; position: fixed; top: 50%; margin-top: -250px; height: 500px; width: 100%; background-color: rgba(0,0,0, 0.85);">
-			<div class="contactusbox" id="contactusbox" style="display: none; text-align: left; width: 340px; margin: 25px auto 0 auto;">
+			<div class="contactusbox" id="contactusbox" style="display: none; text-align: left; width: 340px; margin: 68px auto 0 auto;">
 				<p style="color: #97c93e; float: left; font-size: 24px;">Contact Us</p>
 				<a href="#" class="closelink"><img style="float: right; position: relative; top: 2px; right: -10px;" src="/images/x.png" width="16" height="15" /></a>
 				<?php echo do_shortcode( '[contact-form-7 id="39" title="contact us"]' ); ?>
